910bf9af2a931de060856bd063ae1b4d214f5d5e
BARKHAC <49210263+BARKHAC@users.noreply.github.com>
2025-06-30 18:31:29 -0400

    added navbar using layout, added links for pages, and individual article params
    1. through urlParams using name, allowed for accessing individual articles.
    2. used a layout page and router to maintain consistent navbar and future common components.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,11 +4,34 @@ import {
 } from 'react-router-dom'
 import './App.css'
 import HomePage from './pages/HomePage'
+import AboutPage from './pages/AboutPage'
+import ArticlesList from './pages/ArticlesList'
+import ArticlePage from './pages/ArticlePage' 
+import Layout from './Layout'
 
 const routes = [{
-  path: '/',
-  element: <HomePage/>,
+  path:'/',
+  element: <Layout/>,
+  children: [{
+    path: '/',
+    element: <HomePage/>,
+  },
+  {
+    path:'/about',
+    element: <AboutPage/>,
+  },
+  {
+    path:'/articles',   
+    element: <ArticlesList/>, 
+  },
+  {
+    path:'/articles/:name',
+    element:<ArticlePage/>,
+
+  }]
 }]
+
+
 
 const router = createBrowserRouter(routes);
 

3cf70df10937bbb2f38dd0cbebc995eb71ed4313
BARKHAC <49210263+BARKHAC@users.noreply.github.com>
2025-06-30 18:55:51 -0400

    using js logic to de structure params and display using tags

--- frontend/src/pages/ArticlePage.jsx
+++ frontend/src/pages/ArticlePage.jsx
@@ -1,8 +1,13 @@
 import { useParams } from 'react-router-dom';
+import articles from '../article-content';
 
 export default function ArticlePage(){
     const {name} = useParams();
+    const article = articles.find(a=>a.name === name);
     return(
-        <h1>This is the {name} article.</h1>
+        <>
+             <h1>{article.title}</h1>
+             {article.content.map(p => <p key={p}>{p}</p>)}
+        </>
     )
 }

f2d9dade4a4003e0fbb161c84ef265e897fca4bd
BARKHAC <49210263+BARKHAC@users.noreply.github.com>
2025-07-04 15:46:08 -0400

    Update ArticlesList.jsx

--- frontend/src/pages/ArticlesList.jsx
+++ frontend/src/pages/ArticlesList.jsx
@@ -1,5 +1,16 @@
+import articles from '../article-content'
+import Link from 'react-router-dom';
+
 export default function ArticlesList(){
     return(
-        <h1>This is the articles list page.</h1>
+        <>
+            <h1>Articles</h1>
+            {articles.map(a=>{
+                <Link key={a.name}to={'/articles'+a.name}>
+                <h3>{a.title}</h3>
+                <p>{a.content[0].substring(0,150)}</p>
+                </Link>
+            })}
+        </>
     )
 }

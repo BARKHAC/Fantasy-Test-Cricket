11099b18475affe55c2cfd86eb4c365b1d44d1e5
BARKHAC <49210263+BARKHAC@users.noreply.github.com>
2025-08-18 18:25:04 -0400

    added comments endpoint by first defining comments in memory array, and creating an endpoint for comments
    put api
    destructures name, points to article using name
    takes the body esp postedBy,text
    and pushes the body into article
    sends response as updated json

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,9 +3,9 @@ import express from 'express'
 const app = express();
 
 const articleInfo = [
-    {name:'learn-react',upvotes:0},
-    {name:'learn-node',upvotes:0},
-    {name:'mongodb',upvotes:0},
+    {name:'learn-react',upvotes:0, comments:[]},
+    {name:'learn-node',upvotes:0,comments:[]},
+    {name:'mongodb',upvotes:0,comments:[]},
 ]
 
 app.use(express.json())
@@ -26,9 +26,21 @@ app.post('/api/articles/:name/upvote', (req,res)=>{
     const article = articleInfo.find(a => a.name === req.params.name)
     article.upvotes+=1
 
-    res.send('Hooray! The article ' + req.params.name + ' now has ' + article.upvotes + ' upvotes now')
+    res.json(article)
 })
 
+app.post('/api/articles/:name/comments', (req,res)=>{
+        const name = req.params.name;
+        const {postedBy,text} = req.body;
+
+        const article = articleInfo.find(a=>a.name === name)
+
+        article.comments.push({
+            postedBy,
+            text
+        })
+        res.json(article)
+})
 
 app.listen(8000, function(){
     console.log('Server is listening on port 8000')

0565af1a7b77ce7a9d68c59f950beac597bfd223
BARKHAC <49210263+BARKHAC@users.noreply.github.com>
2025-08-18 17:18:38 -0400

    created backend, added test endpoints and tested with postman, added upvote endpoint
    added upvote endpoint with array memory for now

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,9 +2,33 @@ import express from 'express'
 
 const app = express();
 
-app.get('/hello', function(req,res){
-    res.send('Hello!')
+const articleInfo = [
+    {name:'learn-react',upvotes:0},
+    {name:'learn-node',upvotes:0},
+    {name:'mongodb',upvotes:0},
+]
+
+app.use(express.json())
+
+// app.get('/hello', function(req,res){
+//     res.send('Hello from GET endpoint!')
+// })
+
+// app.get('/hello/:name', function(req,res){
+//     res.send('Hello, ' + req.params.name)
+// })
+
+// app.post('/hello',function(req,res){
+//     res.send('Hello, '+ req.body.name + ' from POST endpoint!')
+// })
+
+app.post('/api/articles/:name/upvote', (req,res)=>{
+    const article = articleInfo.find(a => a.name === req.params.name)
+    article.upvotes+=1
+
+    res.send('Success! The article ' + req.params.name + ' now has ' + article.upvotes + ' upvotes now')
 })
+
 
 app.listen(8000, function(){
     console.log('Server is listening on port 8000')
